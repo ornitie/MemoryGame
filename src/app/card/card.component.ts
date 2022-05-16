3fd017f70c147dc34c2b19bb6d0bb943fc30d016
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2022-05-15 19:18:20 -0500

    Added card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+        var x = document.getElementsByName('card')[0];
+
+        function print(){ 
+            document.getElementsByName('card')[0].classList.toggle('flipped');
+        }
+
+        x.addEventListener('click', print, false);
+
+    }
+
+}
