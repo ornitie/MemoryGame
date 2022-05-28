0086d84b3702689944314ac8fd9925b8a7875101
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2022-05-28 14:00:00 -0500

    Added Question Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
